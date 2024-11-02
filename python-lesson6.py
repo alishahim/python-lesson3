2aaf59340591ad0107ca07e00b38f8df5801c603
nargiza <ravshankhasan@gmail.com>
2024-11-01 20:45:41 -0400

    add file handling

---
+++ python-lesson6.py
@@ -0,0 +1,8 @@
+#with open("example.txt","w") as file:
+ #   file.write("This is devops")
+
+# this line will append following text to example.txt file
+with open("example.txt","a") as file:
+        file.write("Another text here!\n")
+
+        
